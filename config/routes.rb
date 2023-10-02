6928e1f7f9445f14d702320cd9c0cd17623d2cdb
Pinja <pinja.salomaki@gmail.com>
2023-10-02 17:57:49 +0300

    added ratings

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   resources :beers
   resources :breweries
+
+  root 'breweries#index'
+
+  get 'ratings', to: 'ratings#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
