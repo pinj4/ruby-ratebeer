1967e50360d56f648e91142245662ddc78491768
Pinja <pinja.salomaki@gmail.com>
2023-10-03 17:30:32 +0300

    new rating

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,9 @@ Rails.application.routes.draw do
   root 'breweries#index'
 
   get 'ratings', to: 'ratings#index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get 'ratings/new', to:'ratings#new'
+
+  post 'ratings', to: 'ratings#create'
+
 end
