05bce14a42afcf454582bf3e42ee0b0fba51b885
Pinja <pinja.salomaki@gmail.com>
2023-10-03 17:02:57 +0300

    ratings

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,4 +1,9 @@
 class Beer < ApplicationRecord
     belongs_to :brewery
     has_many :ratings
+
+    def average_rating
+        ratings = self.ratings.map{ |r| r.score }
+        (ratings.sum(0) / ratings.size).to_f
+    end
 end
