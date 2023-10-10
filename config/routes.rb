12ecdca42a1e611172b474858b997efc1302e61a
Pinja <pinja.salomaki@gmail.com>
2023-10-10 12:40:40 +0300

    users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :users
   resources :beers
   resources :breweries
 
@@ -8,4 +9,10 @@ Rails.application.routes.draw do
   # get 'ratings/new', to:'ratings#new'
   # post 'ratings', to: 'ratings#create'
   resources :ratings, only: %i[index new create destroy]
+  resource :session, only: [:new, :create, :destroy]
+
+  get 'signup', to: 'users#new'
+
+  get 'signin', to: 'sessions#new'
+  delete 'signout', to: 'sessions#destroy'
 end
